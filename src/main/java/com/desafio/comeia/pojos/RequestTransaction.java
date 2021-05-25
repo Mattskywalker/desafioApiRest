bf93e2f93d5302dfa5b76630f2369fa0450e4bcc
Mattskywalker <mateusejesus@hotmail.com.br>
2021-05-25 18:27:50 -0300

    classe modelo para requisição de transferencia de valores;

---
+++ src/main/java/com/desafio/comeia/pojos/RequestTransaction.java
@@ -0,0 +1,38 @@
+package com.desafio.comeia.pojos;
+
+public class RequestTransaction {
+
+    private String accountDebitNumber;
+    private String accountCreditNumber;
+    private double transferValue;
+
+    public RequestTransaction(String accountDebitNumber, String accountCreditNumber, double transferValue) {
+        this.accountDebitNumber = accountDebitNumber;
+        this.accountCreditNumber = accountCreditNumber;
+        this.transferValue = transferValue;
+    }
+
+    public String getAccountDebitNumber() {
+        return accountDebitNumber;
+    }
+
+    public void setAccountDebitNumber(String accountDebitNumber) {
+        this.accountDebitNumber = accountDebitNumber;
+    }
+
+    public String getAccountCreditNumber() {
+        return accountCreditNumber;
+    }
+
+    public void setAccountCreditNumber(String accountCreditNumber) {
+        this.accountCreditNumber = accountCreditNumber;
+    }
+
+    public double getTransferValue() {
+        return transferValue;
+    }
+
+    public void setTransferValue(double transferValue) {
+        this.transferValue = transferValue;
+    }
+}
