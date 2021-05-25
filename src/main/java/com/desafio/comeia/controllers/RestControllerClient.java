f4b26f74c2a2c41e7c46c3ace3b7bea1e7648648
Mattskywalker <mateusejesus@hotmail.com.br>
2021-05-25 12:48:44 -0300

    adicionada dependencia do swagger no pom.xml,  e documentação da parte de clientes, concluida;

--- src/main/java/com/desafio/comeia/controllers/RestControllerClient.java
+++ src/main/java/com/desafio/comeia/controllers/RestControllerClient.java
@@ -3,6 +3,8 @@ package com.desafio.comeia.controllers;
 import com.desafio.comeia.pojo.Client;
 import com.desafio.comeia.repositories.ClientRepository;
 import com.desafio.comeia.repositories.ClientRepositoryInterface;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,39 +12,44 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API REST clientes")
+@CrossOrigin(origins = "*")
 public class RestControllerClient {
 
     ClientRepositoryInterface clientRepository = new ClientRepository();
 
     @GetMapping("/clientes")
     @ResponseBody
+    @ApiOperation(value = "Este metodo retorna uma lista de clientes do banco de dados")
     public List<Client> index(){
         List<Client> a = clientRepository.getAll();
         return a;
     }
 
     @GetMapping("/cliente/{id}")
+    @ApiOperation(value = "Este metodo retorna um cliente do BD através do seu id")
     public Client show(@PathVariable(value = "id") String id){
         return clientRepository.getByID(id);
     }
 
     @PostMapping("/cliente")
+    @ApiOperation(value = "Este metodo salva clientes no banco de dados e depois disso o retorna")
     public Client create(@RequestBody Client client){
         return clientRepository.save(client);
     }
 
     @DeleteMapping("/cliente")
+    @ApiOperation(value = "Este metodo deleta clientes do banco de dados")
     public void destroy(@RequestBody Client client){
         clientRepository.delete(client);
     }
 
     @PutMapping("/cliente")
+    @ApiOperation(value = "Este metodo faz alterações nos dados dos clientes do banco de dados")
     public Client update(@RequestBody Client client){
 
        return clientRepository.update(client);
 
     }
-
-
 
 }
