ca428ab52b0a1b8a5f38637db32fbc40e1fddee6
Mattskywalker <mateusejesus@hotmail.com.br>
2021-05-26 17:02:03 -0300

    main Limpa, os clientes e contas usados para teste agora são criados por meio dos testes;

--- src/main/java/com/desafio/comeia/ComeiaApplication.java
+++ src/main/java/com/desafio/comeia/ComeiaApplication.java
@@ -18,37 +18,6 @@ public class ComeiaApplication {
 
 	public static void main(String[] args) {
 
-		ClientRepositoryInterface clientRepository = new ClientRepository();
-
-		Client client = new Client("1", Type.PF,"123.627.134-31",
-				"Mattskywalker","Caruaru","81 993146584");
-
-		Client client1 = new Client("2", Type.PJ,"123.627.134-32",
-				"Cliente 1","Caruaru","81 883146584");
-
-		clientRepository.save(client);
-		clientRepository.save(client1);
-
-		List<Account> accounts = new ArrayList<>();
-
-		accounts.add(new Account("1",client,"12345",1000));
-		accounts.add(new Account("2",client,"123",1000));
-
-
-		/*clientRepository.save(new Client("3", Type.PJ,"908.227.174-32",
-				"Lewis Hamilton","England","81 883146864",));*/
-
-
-		clientRepository.update(new Client("1", Type.PJ,"123.627.134-32",
-				"ClienteNovo","Caruaru","81 883146584",accounts));
-
-		BankAccountRepositoryInterface bankAccountRepository = new BankAccountRepository();
-
-		accounts.stream().forEach(account -> {bankAccountRepository.save(account);});
-
-
-
-
 		SpringApplication.run(ComeiaApplication.class, args);
 	}
 
